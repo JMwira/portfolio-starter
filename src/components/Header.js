d4ee547aa7f87e230e27da375689a91da1aef893
JMwira <jaymwira@gmail.com>
2023-03-10 15:12:23 +0530

    header-00

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Logo from "../assets/logo.svg"
 
 const Header = () => {
-  return <div id="home" className='bg-pink/300' >
-    Header
-  </div>;
+  return <header id="home" className='py-8' >
+    <div className='container mx-auto' >
+      <div className='flex justify-between items-center'>
+        {/* logo */}
+        <a href="#home" >
+          <img src={Logo}/>
+        </a>
+        {/* button */}
+        <button className='btn btn-sm' >
+          work with me
+        </button>
+      </div>
+    </div>
+  </header>;
 };
 
 export default Header;
